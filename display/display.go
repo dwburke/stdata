b98eaee26c7fedfb63ed7d6ca0f355e4be29a63a
Dan Burke <dburke@addictmud.org>
2019-03-08 13:41:43 -0500

    display from file

--- display/display.go
+++ display/display.go
@@ -1,8 +1,14 @@
 package display
 
 import (
-	"fmt"
+	//"fmt"
+	"os"
+	"text/template"
 	"time"
+
+	"github.com/spf13/viper"
+
+	"github.com/dwburke/stdata/db"
 )
 
 func init() {
@@ -11,8 +17,45 @@ func init() {
 
 func Run() {
 	for {
-		fmt.Println(time.Now().Format("20060102150405"))
-		fmt.Println(time.Now().Format("20060102150405"))
+
+		t := template.New("display.tmpl").Funcs(template.FuncMap{
+			"viperGetString": func(key string) string {
+				return viper.GetString(key)
+			},
+			"DoorLock": func(key string) string {
+				value := db.LevelDBGet("topic:" + key)
+				return value
+			},
+			"DoorState": func(key string) string {
+				value := db.LevelDBGet("topic:" + key)
+				return value
+			},
+		})
+
+		t2, err := t.ParseFiles("display.tmpl") //setp 1
+		if err != nil {
+			panic(err)
+		}
+
+		//t2.Execute(w, "Hello World!") //step 2
+
+		args := map[string]interface{}{
+			"timestamp": time.Now().Format("Mon Jan 2 15:04:05 MST 2006"),
+			//"timestamp": time.Now().Format("20060102150405"),
+			//"Agent": agent,
+		}
+
+		if err := t2.Execute(os.Stdout, args); err != nil {
+			panic(err)
+		}
+
+		//fmt.Println(time.Now().Format("20060102150405"))
+		//fmt.Println("================\n")
+		//fmt.Print(  "Front   : ")
+		//fmt.Print(  "Garage  : ")
+		//fmt.Print(  "Sun Room: ")
+		//fmt.Print(  "Patio   : ")
+		//fmt.Print(  "Office  : ")
 
 		select {
 		case <-time.After(1 * time.Second):
