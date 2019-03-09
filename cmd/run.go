7c2811a37224508a76b3645e209e8dee7652ff8f
Dan Burke <dburke@addictmud.org>
2019-03-09 09:36:50 -0500

    store history with time uuid

--- cmd/run.go
+++ cmd/run.go
@@ -1,9 +1,12 @@
 package cmd
 
 import (
+	"time"
+
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+	"github.com/streadway/simpleuuid"
 
 	"github.com/dwburke/stdata/db"
 	"github.com/dwburke/stdata/display"
@@ -21,8 +24,15 @@ var runCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 
 		go stmqtt.Listen("smartthings/#", func(client mqtt.Client, msg mqtt.Message) {
-			log.Printf("- [%s] %s\n", msg.Topic(), string(msg.Payload()))
+			uuid, err := nextId()
+			if err != nil {
+				panic(err)
+			}
+
+			log.Printf("- [%s] [%s] %s\n", uuid, msg.Topic(), string(msg.Payload()))
+
 			db.LevelDBSet("topic:"+msg.Topic(), string(msg.Payload()))
+			db.LevelDBSet(uuid+":"+msg.Topic(), string(msg.Payload()))
 			display.Refresh()
 		})
 
@@ -31,4 +41,13 @@ var runCmd = &cobra.Command{
 		done := make(chan bool)
 		<-done
 	},
+}
+
+func nextId() (string, error) {
+	uuid, err := simpleuuid.NewTime(time.Now())
+	if err != nil {
+		return "", err
+	}
+
+	return uuid.String(), nil
 }
